92d5e0d91c2e529db889c0239c06b93309fb2e3a
awais khan <owikhan91@gmail.com>
2023-09-13 01:44:51 +0500

    progress

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CategoryService, ModelService, ProductService, AppService } from '@services/index'
 
+
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
@@ -9,6 +10,7 @@ export class BsNavbarComponent {
 
   allCategories: any;
   allProducts: any;
+  allProductsByCategory: any=[];
 
   constructor(
     private modelService: ModelService,
@@ -17,6 +19,7 @@ export class BsNavbarComponent {
 
     ngOnInit(){
       this.getAllCategory();
+      this.getAllProduct();
     }
 
     getAllCategory(){
@@ -24,8 +27,13 @@ export class BsNavbarComponent {
       this.appService.getService(url).subscribe(res => {
         this.allCategories = res;
         console.log('this.allCategories', this.allCategories);
-        for (var i = 0; i < this.allCategories.length; i++) {
-          this.allCategories[i] = this.allCategories[i].charAt(0).toUpperCase() + this.allCategories[i].slice(1);
+        // for (var i = 0; i < this.allCategories.length; i++) {
+        //   this.allCategories[i] = this.allCategories[i].charAt(0).toUpperCase() + this.allCategories[i].slice(1);
+        // }
+        for (let i = 0; i<this.allCategories.length; i++) {
+          let result = this.getProductsByCategory(this.allCategories[i]);
+          this.allProductsByCategory.push(result);
+          console.log('this.result',result)
         }  
       })
     }
@@ -35,6 +43,16 @@ export class BsNavbarComponent {
     this.appService.getService(url).subscribe(res => {
       this.allProducts = res;
       console.log('this.allProducts', this.allProducts);
+
       })
+    }
+
+    getProductsByCategory(category:string){
+      let url = 'products/category/'+category;
+      let result;
+      this.appService.getService(url).subscribe(res => {
+        result= res;
+      })
+      return result
     }
 }
